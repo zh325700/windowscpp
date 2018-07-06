9169a1950627b601ac1ee4070dd0230109bb32ca
Shuaigehan <zh325700@gmail.com>
2018-07-06 12:41:18 +0200

    add some 大疆笔试

---
+++ 剑指offer/chapter2/operatorOverwrite.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+class CMYString{
+public:
+CMYString(char *pData){
+  m_pData = pData;
+}
+CMYString(const CMYString& str){
+  m_pData = str.m_pData;
+}
+CMYString& operator =(const CMYString& str);
+~CMYString(){
+
+}
+char * getm_pData(){
+  return m_pData;
+}
+private:
+  char *m_pData;
+};
+
+CMYString& CMYString::operator =(const CMYString& str){
+  if(this != &str){
+     CMYString temp(str);
+
+     temp.m_pData = m_pData;
+     m_pData = str.m_pData;
+
+  }
+
+  return *this;
+}
+
+
+int main(){
+  CMYString A("qunimade");
+  CMYString B("wohenhao");
+  CMYString C("budeliao");
+  CMYString D("zenmshuo");
+  A=B=C;
+  C=D;
+  cout<<A.getm_pData()<<endl;
+  cout<<C.getm_pData()<<endl;
+  return 0;
+}
--- 大疆笔试/Chahua.cpp
+++ 大疆笔试/Chahua.cpp
@@ -1,9 +1,29 @@
 //一共N种花，插花需要每次选M种，每种R支。第二行输入每种花个数，求最多有多少种插花方法。
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void example(int *acWelcome, int size){
+    cout<<sizeof(acWelcome)<<endl;
+    cout<<sizeof(acWelcome[0])<<endl;
+    while(size--){
+      cout<<*(acWelcome+size)<<'\t';
+    }
+    return;
+}
+
 int main(){
-  
-  return 0;
+    vector<char> charVec(10,'a');
+    cout<<"The size of the vector is: "<<charVec.size()<<endl;
+    for(auto n: charVec){
+      cout<<n<<'\t';
+    }
+    cout<<endl;
+    int acWelcome[]={1,2,3,4,5,6,7,7,6,6,6,6,6};
+    example(acWelcome,sizeof(acWelcome)/sizeof(acWelcome[0]));
+    cout<<sizeof(acWelcome)<<endl;
+    cout<<sizeof(acWelcome[0])<<endl;
+    cout<<sizeof(acWelcome)/sizeof(acWelcome[0])<<endl;
+    return 0;
 }
