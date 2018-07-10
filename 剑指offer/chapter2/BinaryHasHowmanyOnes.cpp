bf6a8c057de54e38a8735025094f71fac4c7440a
Shuaigehan <zh325700@gmail.com>
2018-07-10 15:30:17 +0200

    add moving flag to deal with count how many 1 in a integer using bianry coding

--- 剑指offer/chapter2/BinaryHasHowmanyOnes.cpp
+++ 剑指offer/chapter2/BinaryHasHowmanyOnes.cpp
@@ -13,10 +13,22 @@ int countHowMantones(int n){
   return count;
 }
 
+int countWithMovingFlag(int n){
+  int count =0;
+  int flag =1;
+  while(flag){
+    if(n & flag){
+      count ++;
+    }
+    flag = flag <<1;
+  }
+  return count;
+}
+
 int main(){
   int n=0;
   while(cin>>n){
-    cout<< countHowMantones(n)<<endl;
+    cout<< countWithMovingFlag(n)<<endl;
   }
   return 0;
 }
