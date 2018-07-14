5dea60d64854e2e408da03c60254ae21cd2267a4
Shuaigehan <zh325700@gmail.com>
2018-07-14 16:34:00 +0200

    final functionPointors

--- Amadeus/面试曾经出现过得问题/functionPointor.cpp
+++ Amadeus/面试曾经出现过得问题/functionPointor.cpp
@@ -9,15 +9,26 @@ void fun(int a)
     printf("Value of a is %d\n", a);
 }
 
+void func_2(int a){
+  cout<<"I am function two"<<a<<endl;
+}
+
 int notFun(int n){
   return n;
+}
+
+void onclick(void (*func_ptr)(int)){
+  (*func_ptr)(123);   // call back to teh function
 }
 
 int main()
 {
     // fun_ptr is a pointer to function fun()
-    void (*fun_ptr)(int) = &fun;
+    void (*func_ptr[])(int) = {&fun,&func_2};
+
     int (*function_ptr)(int) = &notFun;
+
+
 
     /* The above line is equivalent of following two
        void (*fun_ptr)(int);
@@ -27,6 +38,8 @@ int main()
     // Invoking fun() using fun_ptr
     // (*fun_ptr)(10);
     cout<<(*function_ptr)(100)<<endl;
+    onclick(func_ptr[0]);
+    onclick(func_ptr[1]);
 
     return 0;
 }
